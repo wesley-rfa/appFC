ba7961fe0c5369ca7f06c7dee3df7a7075a52c5e
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2022-02-27 17:36:28 -0300

    estilizando header da home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,18 +4,29 @@ import { StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import {
-  Container
+  Container, Header,
+  UserInfo, UserGreetting,
+  UserName, IconLogout,
+  UserWrapper
 } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 export default function Home() {
   const navigation = useNavigation<any>();
-
-
+  const { user } = useAuth()
 
   return (
     <Container>
-      <StatusBar barStyle="dark-content" />
-
+      <StatusBar barStyle="light-content" />
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserGreetting>Olá,</UserGreetting>
+            <UserName>{user.userName}</UserName>
+          </UserInfo>
+          <IconLogout name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,42 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex:1;
   background: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  height: ${RFValue(113)}px;
+  background: ${({ theme }) => theme.colors.primary};
+  padding: 0px ${RFValue(24)}px ${RFValue(7)}px ${RFValue(24)}px;
+  
+  
+`;
+
+export const UserWrapper = styled.View`
+  margin-top: ${getStatusBarHeight() + 25}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content:space-between;
+`;
+
+export const UserInfo = styled.View`
+`;
+
+export const UserGreetting = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+`;
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.shape};
+`;
+export const IconLogout = styled(Feather)`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(24)}px;
 `;
