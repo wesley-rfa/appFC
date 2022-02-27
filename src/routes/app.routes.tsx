ab8399f38b2932e9f1397408bd75608a32ad7bb3
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2022-02-27 15:45:02 -0300

    estilizando tab navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
+import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme()
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="Início" component={Home} />
-      <Screen name="Cadastrar" component={Home} />
-      <Screen name="Resumo" component={Home} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 80,
+          paddingVertical: 12
+        }
+      }}
+    >
+      <Screen
+        name="Início"
+        component={Home}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons name="format-list-bulleted" size={size} color={color} />
+          )
+        }}
+      />
+
+      <Screen
+        name="Cadastrar"
+        component={Home}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <FontAwesome5 name="user-alt" size={size} color={color} />
+          )
+        }}
+      />
+
+      <Screen
+        name="Resumo"
+        component={Home}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          )
+        }}
+      />
     </Navigator>
   )
 }
